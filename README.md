2fc7eff24eb012222fbc2061c07bc4170764769e
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-09-21 10:13:19 -0400

    Initialize repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# SCorrelatorUtilities
+
+This is a namespace which collects various useful types, methods, etc. used throughout the sPHENIX Cold QCD Energy-Energy Correlator analysis.
---
+++ SCorrelatorUtilities.h
@@ -0,0 +1,18 @@
+// ----------------------------------------------------------------------------
+// 'SCorrelatorUtilities.h'
+// Derek Anderson
+// 09.21.2023
+//
+// A namespace to collect various useful structs, methods, etc.
+// for the sPHENIX Cold QCD EEC analysis.
+// ----------------------------------------------------------------------------
+
+
+
+namespace SCorrelatorUtilities {
+
+  /* TODO stuff will go here */
+
+}  // end namespace
+
+// end ------------------------------------------------------------------------
