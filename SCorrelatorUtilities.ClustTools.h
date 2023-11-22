4589adcbcd31a56d65a8e9a6d86abdd12605d915
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-11-22 13:16:37 -0500

    Squash ClustInfo definition into ClustTools headers

--- SCorrelatorUtilities.ClustTools.h
+++ SCorrelatorUtilities.ClustTools.h
@@ -18,6 +18,45 @@ using namespace findNode;
 namespace SColdQcdCorrelatorAnalysis {
   namespace SCorrelatorUtilities {
 
+    // ClustInfo definition ---------------------------------------------------
+
+    /* TODO
+     *   - add overloaded <, > operators for applying cuts
+     *   - add SetInfo(RawClusterContainer::ConstIterator*) method
+     *   - add TypeDef for Cluster = RawClusterContainer::ConstIterator
+     *   - add enums for calo to namespace
+     */
+    struct ClustInfo {
+
+      // data members
+      int    m_sys = -1;
+      double m_pt  = -999.;
+      double m_eta = -999.;
+      double m_phi = -999.;
+      double m_ene = -999.;
+
+      void SetInfo(const int sys, const double pt, const double eta, const double phi, const double ene) {
+        m_sys = -1;
+        m_pt  = pt;
+        m_eta = eta;
+        m_phi = phi;
+        m_ene = ene;
+        return;
+      }  // end 'SetInfo(int, double, double, double, double)'
+
+      void Reset() {
+        m_sys = -1;
+        m_pt  = -999.;
+        m_eta = -999.;
+        m_phi = -999.;
+        m_ene = -999.;
+        return;
+      }  // end 'Reset()'
+
+    };  // end ClustInfo def
+
+
+
     // cluster methods --------------------------------------------------------
 
     RawClusterContainer* GetClusterStore(PHCompositeNode* topNode, const TString sNodeName) {
