456187ea064cb258e696a6aa5a190a23a59f93ef
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-12-30 18:26:31 -0500

    Add map of pid onto charge

--- Constants.h
+++ Constants.h
@@ -9,6 +9,8 @@
 
 #pragma once
 
+// c++ utilities
+#include <map>
 
 
 namespace SColdQcdCorrelatorAnalysis {
@@ -26,8 +28,41 @@ namespace SColdQcdCorrelatorAnalysis {
     const uint16_t NTpcLayer  = 48;
     const uint16_t NTpcSector = 12;
 
-    // pion mass in GeV/c^2
+    // charged pion mass in GeV/c^2
     const double MassPion = 0.140;
+
+    // map of PID onto charges
+    map<int, float> mapPidOntoCharge = {
+      {11, -1.},
+      {12, 0.},
+      {13, -1.},
+      {14, 0.},
+      {15, -1.},
+      {16, 0.},
+      {22, 0.},
+      {23, 0.},
+      {24, 1.},
+      {111, 0.},
+      {130, 0.},
+      {211, 1.},
+      {221, 0.},
+      {310, 0.},
+      {321, 1.},
+      {421, 0.},
+      {431, 1.},
+      {441, 1.},
+      {2112, 1.},
+      {2212, 1.},
+      {3112, -1.},
+      {3122, 0.},
+      {3212, 0.},
+      {3222, 1.},
+      {3322, 0.},
+      {700201, 0.},
+      {700202, 2.},
+      {700301, 0.},
+      {700302, 3.}
+    };
 
   }  // end constants
 
