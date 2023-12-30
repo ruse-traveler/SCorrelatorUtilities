d1c80a62647aebcadb20341513fcfa2b8b1a74d1
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-12-29 19:58:00 -0500

    Add skeleton for interfaces header

---
+++ Interfaces.h
@@ -0,0 +1,22 @@
+// ----------------------------------------------------------------------------
+// 'Interfaces.h'
+// Derek Anderson
+// 12.29.2023
+//
+// Various interfaces between objects and data structures (e.g.
+// TTree's) are collected here.
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+
+
+namespace SColdQcdCorrelatorAnalysis {
+  namespace SCorrelatorUtilities {
+
+    // correlator object-data structure interfaces ----------------------------
+
+  }  // end SCorrelatorUtilities namespace
+}  // end SColdQcdCorrealtorAnalysis namespace
+
+// end ------------------------------------------------------------------------
