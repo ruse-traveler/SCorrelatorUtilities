389b8a34415b86e190b155b7fa35624b3f470cc0
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-12-30 23:31:21 -0500

    Fill in ParInfo and methods

--- GenTools.h
+++ GenTools.h
@@ -9,6 +9,15 @@
 
 #pragma once
 
+// c++ utilities
+#include <cmath>
+// hepmc includes
+#include <HepMC/GenEvent.h>
+#include <HepMC/GenVertex.h>
+#include <HepMC/GenParticle.h>
+#include <phhepmc/PHHepMCGenEvent.h>
+#include <phhepmc/PHHepMCGenEventMap.h>
+
 // make common namespaces implicit
 using namespace std;
 using namespace findNode;
@@ -17,6 +26,139 @@ using namespace findNode;
 
 namespace SColdQcdCorrelatorAnalysis {
   namespace SCorrelatorUtilities {
+
+    // ParInfo definition -----------------------------------------------------
+
+    struct ParInfo {
+
+      int   pid     = -1;
+      int   status  = 0;
+      int   barcode = -1;
+      int   embedID = -1;
+      float charge  = -999.;
+      float mass    = -999.;
+      float eta     = -999.;
+      float phi     = -999.;
+      float ene     = -999.;
+      float px      = -999.;
+      float py      = -999.;
+      float pz      = -999.;
+      float pt      = -999.;
+      float vx      = -999.;
+      float vy      = -999.;
+      float vz      = -999.;
+
+      void SetInfo(const HepMC::GenParticle* particle) {
+        pid     = particle -> pdg_id();
+        status  = particle -> status();
+        barcode = particle -> barcode();
+        charge  = mapPidOntoCharge[pid];
+        mass    - particle -> momentum().m();
+        eta     = particle -> momentum().eta();
+        phi     = particle -> momentum().phi();
+        ene     = particle -> momentum().e();
+        px      = particle -> momentum().px();
+        py      = particle -> momentum().py();
+        pz      = particle -> momentum().pz();
+        pt      = particle -> momentum().perp();
+        vx      = particle -> production_vertex() -> position().x();
+        vy      = particle -> production_vertex() -> position().y();
+        vz      = particle -> production_vertex() -> position().z();
+        return;
+      };
+
+      void Reset() {
+        pid     = -1;
+        status  = 0;
+        barcode = -1;
+        embedID = -1;
+        charge  = -999.;
+        mass    = -999.;
+        eta     = -999.;
+        phi     = -999.;
+        ene     = -999.;
+        px      = -999.;
+        py      = -999.;
+        pz      = -999.;
+        pt      = -999.;
+        vx      = -999.;
+        vy      = -999.;
+        vz      = -999.;
+        return;
+      };
+
+      static vector<string> GetListOfMembers() {
+        vector<string> members = {
+          "pid",
+          "status",
+          "barcode",
+          "embedID",
+          "charge",
+          "mass",
+          "eta",
+          "phi",
+          "ene",
+          "px",
+          "py",
+          "pz",
+          "pt",
+          "vx",
+          "vy",
+          "vz"
+        };
+        return members;
+      }  // end 'GetListOfMembers()'
+
+      // overloaded < operator
+      friend bool operator<(const ParInfo& lhs, const ParInfo& rhs) {
+
+        // note that some quantities aren't relevant for this comparison
+        const bool isLessThan = (
+          (lhs.eta  < rhs.eta)  &&
+          (lhs.phi  < rhs.phi)  &&
+          (lhs.ene  < rhs.ene)  &&
+          (lhs.px   < rhs.px)   &&
+          (lhs.py   < rhs.py)   &&
+          (lhs.pz   < rhs.pz)   &&
+          (lhs.pt   < rhs.pt)
+        );
+        return isLessThan;
+
+      }  // end 'operator>(ParInfo&, ParInfo&)'
+
+      // overloaded > operator
+      friend bool operator>(const ParInfo& lhs, const ParInfo& rhs) {
+
+        // note that some quantities aren't relevant for this comparison
+        const bool isGreaterThan = (
+          (lhs.eta  > rhs.eta)  &&
+          (lhs.phi  > rhs.phi)  &&
+          (lhs.ene  > rhs.ene)  &&
+          (lhs.px   > rhs.px)   &&
+          (lhs.py   > rhs.py)   &&
+          (lhs.pz   > rhs.pz)   &&
+          (lhs.pt   > rhs.pt)
+        );
+        return isGreaterThan;
+
+      }  // end 'operator>(ParInfo&, ParInfo&)'
+
+      // overloaded, <=, >= operators
+      inline friend bool operator<=(const ParInfo& lhs, const ParInfo& rhs) {return !(lhs > rhs);}
+      inline friend bool operator>=(const ParInfo& lhs, const ParInfo& rhs) {return !(lhs < rhs);}
+
+      // default ctor/dtor
+      ParInfo()  {};
+      ~ParInfo() {};
+
+      // ctor accepting HepMC::GenParticle
+      ParInfo(const HepMC::GenParticle* particle) {
+        SetInfo(particle);
+      };
+
+    };  // end ParInfo definition
+
+
 
     // generator/mc methods ---------------------------------------------------
 
@@ -77,6 +219,14 @@ namespace SColdQcdCorrelatorAnalysis {
 
 
 
+    bool IsInParAcceptance(const ParInfo& particle, const ParInfo& minimum, const ParInfo& maximum) {
+
+      return ((particle >= minimum) && (particle <= maximum));
+
+    }  // end 'IsInParAcceptance(ParInfo&, ParInfo&, ParInfo&)'
+
+
+
     bool IsFinalState(const int status) {
 
       return (status == 1);
@@ -105,7 +255,7 @@ namespace SColdQcdCorrelatorAnalysis {
 
 
 
-    bool IsOutgoingParton(HepMC::GenParticle* par) {
+    bool IsOutgoingParton(const HepMC::GenParticle* par) {
 
       // grab particle info
       const int pid    = par -> pdg_id();
