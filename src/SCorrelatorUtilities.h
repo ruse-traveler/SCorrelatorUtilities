857e72857796a7f7916ec3d38244dafb8561535a
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-02-16 19:10:20 -0500

    Make convenience header

---
+++ src/SCorrelatorUtilities.h
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------------------
+// 'SCorrelatorUtilities.h'
+// Derek Anderson
+// 02.16.2023
+//
+// All-in-one header for convenience. 
+// ----------------------------------------------------------------------------
+
+#include "CalTools.h"
+#include "Constants.h"
+#include "CstTools.h"
+#include "EvtTools.h"
+#include "FlowTools.h"
+#include "GenTools.h"
+#include "Interfaces.h"
+#include "JetTools.h"
+#include "TrkTools.h"
+#include "VtxTools.h"
+
+// end ------------------------------------------------------------------------
